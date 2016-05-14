a55058def31a5801618ad309d82da231d5e44d85
Nuno Pinheiro <nuno.pinheiro@qub-it.com>
2016-05-14 19:30:44 +0100

    First iteration of react i18n helper for react

---
+++ README.md
@@ -0,0 +1,4 @@
+# react-i18n-helper
+A set of functions help during the i18n (translation) of a react application.
+
+Setup
---
+++ examples/basic-sample/app.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import {changeLocale, i18nEventsListenerMixin, setupI18NConfiguration, i18n } from "../../lib/react-i18n-helper";
+
+var I18NSelectionComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <button onClick={() => changeLocale("pt-PT")}>pt-PT</button>
+        <button onClick={() => changeLocale("en-GB")}>en-GB</button>
+        <button onClick={() => changeLocale("fr-FR")}>fr-FR (Will falback to en-GB)</button>
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  mixins : [i18nEventsListenerMixin],
+  getInitialState : function(){
+    //The initial data should be injected on the page
+    var initialData = {"HelloWorld" : "Hello, World!"};
+    setupI18NConfiguration({clientPath : "/basic-sample/i18n", fallBackLocale : "en-GB", initialState : {"locale" : "en-GB", data : initialData}});
+    return {};
+  },
+  render : function(){
+    return (<div>
+              <I18NSelectionComponent />
+              {i18n("HelloWorld")}
+            </div>);
+  }
+});
+
+ReactDOM.render( <App />, document.getElementById('container'));
